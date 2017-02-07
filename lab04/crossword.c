42f54237b6e864d44e0d015a03858a56c3f37855
Dominic Lesaca <d_lesaca@u.pacific.edu>
2017-02-07 11:09:40 -0800

    added I/O code for calling the puzzle file

--- lab04/crossword.c
+++ lab04/crossword.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 
 int main() {
-	printf("Hello, World");
+	FILE * puzzleFile;
+	char * line = NULL;
+	size_t len = 0;
+	ssize_t read;
+	
+	printf("Hello, World\n");
+	puzzleFile = fopen("crossword_puzzle.txt", "r");
+	while ((read = getline(&line, &len, puzzleFile)) != -1) {
+		printf("%s", line);
+	}
 	return 0;
 }
+
+
