9e73b317ffae115a26e8b432364d99ba93b0b75c
Dominic Lesaca <d_lesaca@u.pacific.edu>
2017-02-09 11:14:02 -0800

    FIle can now read in the puzzle document and outputthe clues to screen

--- lab04/crossword.c
+++ lab04/crossword.c
@@ -1,15 +1,42 @@
 #include <stdio.h>
 
 int main() {
-	FILE * puzzleFile;
-	char * line = NULL;
-	size_t len = 0;
-	ssize_t read;
+	//for reading the file in
+	char filename[256];
+	FILE *file = NULL;
+	//variables for the first row
+	int rows;
+	int cols;
+	int clues;
+	//variables for all other roles	
+	char direction;
+	int startX;
+	int startY;
+	char word[256];
+	char hint[256];
+	//for garbage collection
+	char garbage;
 	
-	printf("Hello, World\n");
-	puzzleFile = fopen("crossword_puzzle.txt", "r");
-	while ((read = getline(&line, &len, puzzleFile)) != -1) {
-		printf("%s", line);
+	printf("Enter filename of the game: ");
+	scanf("%255s", filename);
+	file = fopen(filename, "r");
+	
+	int count = fscanf(file, "%i %i %i\n", &rows, &cols, &clues);
+	//gameboard
+	if(count == 3){
+		for(int i=0; i<clues; i++){
+			count = fscanf(file, "%c %i %i %255s %255[^\n]", 					&direction, &startX, &startY, word, hint);
+			fscanf(file, "%c", &garbage);
+			if(count == 5){
+				printf("%s\n", hint);
+			}
+			else{
+				printf("ERROR: not all variables matched");
+			}
+		}
+	}
+	else{
+		printf("ERROR:  first line of file is invalid");
 	}
 	return 0;
 }
