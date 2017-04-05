488258d1141fe2ff390e75ac6213f759b7af2ad4
Dominic Lesaca <d_lesaca@u.pacific.edu>
2017-04-04 20:48:51 -0700

    Final commit for lab 9

--- lab09/dns.py
+++ lab09/dns.py
@@ -15,6 +15,7 @@
 
 
 from dns_tools import dns  # Custom module for boilerplate code
+from dns_tools import dns_header_bitfields
 
 import argparse
 import ctypes
@@ -24,7 +25,7 @@ import struct
 import sys
 
 def main():
-
+    max_recv = 64 * 1024
     # Setup configuration
     parser = argparse.ArgumentParser(description='DNS client for ECPE 170')
     parser.add_argument('--type', action='store', dest='qtype',
@@ -49,33 +50,99 @@ def main():
     # ---------
     # STUDENT TO-DO
     # ---------
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    except socket.error as error:
+        print('ERROR:  couldnt create socket')
+        print('Description:  ', str(error))
+        sys.exit()
 
 
     # Generate DNS request message
     # ---------
     # STUDENT TO-DO
     # ---------
+    bitfields = dns_header_bitfields()
+    bitfields.qr = 0
+    bitfields.opcode = 0
+    bitfields.aa = 0
+    bitfields.tc = 0
+    bitfields.rd = 1
+    bitfields.ra = 0
+    bitfields.reserved = 0
+    bitfields.rcode = 0
 
+    messageID = 0xfe3c
+    qdcount = 1
+    ancount = 0
+    nscount = 0
+    arcount = 0
+
+    splitname = qname.split('.')
+
+    request = bytearray()
+    request += struct.pack('!H2sHHHH',
+                           messageID,
+                           bytes(bitfields),
+                           qdcount,
+                           ancount,
+                           nscount,
+                           arcount)
+    for x in splitname:
+        request += struct.pack('B', len(x))
+        request += x.encode('ascii')
+    request += struct.pack('B', 0)
+
+    request += struct.pack('B', 0)
+    if qtype == 'A':
+        qtypenum = 1
+    else:
+        qtypenum = 0x001c
+    request += struct.pack('B', qtypenum)
+
+    request += struct.pack('B', 0)
+    request += struct.pack('B', 1)
 
     # Send request message to server
     # (Tip: Use sendto() function for UDP)
     # ---------
     # STUDENT TO-DO
     # ---------
-
+    print('Sending request for ', qname, ', type ', qtype, ', to server ',
+          server_ip, ', port ', port)
+    try:
+        s.sendto(request, server_address)
+    except socket.error as error:
+        print('ERROR:  couldnt send request')
+        print('Description:  ', str(error))
+        sys.exit()
 
     # Receive message from server
     # (Tip: use recvfrom() function for UDP)
     # ---------
     # STUDENT TO-DO
     # ---------
+    res = b''
 
+    try:
+        (raw_bytes, address) = s.recvfrom(4096)
+        res += raw_bytes
+
+    except socket.error as error:
+        print('ERROR:  failed to receive bytes from host')
+        print('Description:  ', str(error))
+        sys.exit()
 
     # Close socket
     # ---------
     # STUDENT TO-DO
     # ---------
-
+    try:
+        s.close()
+    except socket.error as error:
+        print('ERROR:  failed to close socket')
+        print('Description:  ', str(error))
+        sys.exit()
 
     # Decode DNS message and display to screen
     dns.decode_dns(raw_bytes)
